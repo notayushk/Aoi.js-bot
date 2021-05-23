eb00a872f3c96794125af9d37921a08f0c5bd1b2
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-23 16:02:41 +0530

    Create invite.js

---
+++ Commands/General/invite.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "invite",
+  code: `$title[Economy Bot invite links]
+  $description[
+   Recommended: [Click here]($getbotinvite)
+   Admin: [Click here](https://discord.com/api/oauth2/authorize?client_id=BOT_ID&permissions=8&scope=bot)
+  $footer[Thanks for Choosing Me]`
+}
+
