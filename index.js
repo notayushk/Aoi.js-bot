d8b7b91eefc1365a908de8d68a9073fdadf33926
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-23 11:57:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const aoi = require("aoi.js")
 const bot = new aoi.Bot({
-     token: "",
-     prefix: ""
-})
+     token: config.Token,
+     prefix: [config.Prefix_1,config.Prefix_2,config.Prefix_3]
+});
+
 bot.onMessage({
   guildOnly: false //make commands work in dms
-})
+});
 
-bot.loadCommands('./Commmands/') //command handler
+bot.loadCommands('./Commmands/'); //command handler
+bot.status {(
+ text: "${config.Prefix_3} help",
+ type: "LISTENING",
+ time: 10
+});
