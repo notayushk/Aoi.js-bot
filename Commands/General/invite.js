1d4c5ac7787cafd4d3381c36b01ba588592b1917
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-23 19:24:37 +0530

    Update invite.js

--- Commands/General/invite.js
+++ Commands/General/invite.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "invite",
   code: `$title[Economy Bot invite links]
   $description[
-   Recommended: [Click here]($getbotinvite)
+   Recommended: [Click here]($getBotInvite)
    Admin: [Click here](https://discord.com/api/oauth2/authorize?client_id=BOT_ID&permissions=8&scope=bot)
   $footer[Thanks for Choosing Me]`
 }
