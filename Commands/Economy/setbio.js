ae48ffc3301debadfc41359bbcf6ac11925cf879
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-24 10:22:06 +0530

    Create setbio.js

---
+++ Commands/Economy/setbio.js
@@ -0,0 +1,6 @@
+module.exports ({
+name: "setbio",
+code: `
+  $setGlobalUserVar[bio;$noMentionMessage]
+  Successfully changed you bio to `$noMentionMessage`
+  $onlyIf[$noMentionMessage!=;Please provide some text]
