9115900a43e31dbabf85b1485d1a58c405f69626
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-25 12:34:57 +0530

    Update trump.js

--- Commands/Fun/trump.js
+++ Commands/Fun/trump.js
@@ -1,5 +1,7 @@
 // Allows you to make a fake tweet from Donald Trump
 module.exports ({
   name: "trump",
-  code: ``
+  code: `
+    $image[https://api.no-api-key.com/api/v2/trump&text=$message]
+`
 )};
