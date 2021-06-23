eb7a6407818091f6b55d9559c91ed400fd8e9560
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-06-23 21:15:03 +0530

    Delete stop.js

--- stop.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({
-    name: "stop",
-    code: `$color[RANDOM]
-    $description[Stopped the song!]
-    $stopSong
-    $onlyIf[$voiceID!=;{color:RANDOM}{description:You must be in a Voice Channel!}]
-    $onlyIf[$queueLength>0;{color:RANDOM}{description:The queue is empty!}]
-    `
-})
